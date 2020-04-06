2cbb6811bc9d37742981c62301b228cb80bc984e
xaraxx <sara.galvan.o91@gmail.com>
2020-04-05 22:11:28 -0500

    promesas en javascript

--- src/js/home.js
+++ src/js/home.js
@@ -6,3 +6,19 @@ let cambia = "@LeonidasEsteban"
 function cambiarNombre(nuevoNombre) {
   cambia = nuevoNombre
 }
+
+const getUser = new Promise(function(resolve, reject){
+  setTimeout(resolve, 3000)
+  // resolve()
+})
+
+getUser.then(function(){
+  console.log('All is ok!')
+})
+
+getUser.catch(function(){
+  console.log('Sorry! something is wrong!')
+})
+
+
+
