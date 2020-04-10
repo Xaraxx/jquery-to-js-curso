1c012b2ec675e9227e9c9314076cb411dd1ba943
xaraxx <sara.galvan.o91@gmail.com>
2020-04-09 22:51:24 -0500

    adding searching to the api

--- src/js/home.js
+++ src/js/home.js
@@ -51,16 +51,54 @@ function cambiarNombre(nuevoNombre) {
   }
   const $form = document.getElementById('form')
   const $home = document.getElementById('home')
+  const $featuringContainer = document.getElementById('featuring')
 
-  $form.addEventListener('submit', (event) => {
+
+  function setAttributes($element, attributes){
+    for (const attribute in attributes){
+      $element.setAttribute(attribute, attributes[attribute])
+    }
+  }
+
+  const BASE_API = 'https://yts.mx/api/v2/'
+
+  function featuringTemplate(peli){
+    return(
+    `
+    <div class="featuring">
+        <div class="featuring-image">
+          <img src="${peli.medium_cover_image}" width="70" height="100" alt="">
+        </div>
+        <div class="featuring-content">
+          <p class="featuring-title">Pelicula encontrada</p>
+          <p class="featuring-album">${peli.title}</p>
+        </div>
+      </div>
+    `)
+  }
+
+  $form.addEventListener('submit', async (event) => {
     event.preventDefault() // => esto es para evitar que el navegador se recarge con cada búsqueda! 
     
     $home.classList.toggle('search-active')
+    const $loader = document.createElement('img')
+    setAttributes($loader, {
+      src:'src/images/loader.gif',
+      height: 50,
+      width: 50
+    })
+
+    $featuringContainer.append($loader)
+    const data = new FormData($form)
+    const peli = await getData(`${BASE_API}list_movies.json?limit=1&query_term=${data.get('name')}`)
+  
+    const HTMLString = featuringTemplate(peli.data.movies[0])
+    $featuringContainer.innerHTML = HTMLString
   })
 
-  const actionList = await getData('https://yts.mx/api/v2/list_movies.json?genre=action')
-  const dramaList = await getData('https://yts.mx/api/v2/list_movies.json?genre=drama')
-  const animationList = await getData('https://yts.mx/api/v2/list_movies.json?genre=animation')
+  const actionList = await getData(`${BASE_API}list_movies.json?genre=action`)
+  const dramaList = await getData(`${BASE_API}list_movies.json?genre=drama`)
+  const animationList = await getData(`${BASE_API}list_movies.json?genre=animation`)
   
   console.log(actionList, dramaList, animationList)
   // const $home = $('.home .list #item')
@@ -87,6 +125,7 @@ function cambiarNombre(nuevoNombre) {
     html.body.innerHTML = HTMLString;
     return html.body.children[0]
   }
+
   function addEventClick($element){
     $element.addEventListener('click', () => {
       showModal()
@@ -110,11 +149,7 @@ function cambiarNombre(nuevoNombre) {
   renderMovieList(dramaList.data.movies, $dramaContainer)
   renderMovieList(animationList.data.movies, $animationContainer)
   
-
-  const $featuringContainer = document.getElementById('#featuring')
   
-
-
   const $modal = document.getElementById('modal')
   const $overlay = document.getElementById('overlay')
   const $hideModal = document.getElementById('hide-modal')
@@ -127,11 +162,14 @@ function cambiarNombre(nuevoNombre) {
     $overlay.classList.add('active')
     $modal.style.animation = 'modalIn .8s forwards'
   }
-  function showModal(){
+
+  $hideModal.addEventListener('click', hideModal)
+  function hideModal(){
     $overlay.classList.remove('active')
-    $modal.style.animation = 'modalIn .8s forwards'
+    $modal.style.animation = 'modalOut .8s forwards'
   }
-  $hideModal.addEventListener('click', hideModal())
+
+  
 
   // '<div class="primaryPlaylistItem">' +
   //   '<div class="primaryPlaylistItem-image">' +
